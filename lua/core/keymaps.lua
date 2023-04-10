87402dd12011087f18b78f374b762ce8cb41d044
Oscar Flores <oscar.floresl@udem.e.du>
2023-04-10 13:48:59 -0600

    Basic keybindings

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,8 @@
+-- Window navigation when using :split and :vsplit
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+-- Forget search
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
