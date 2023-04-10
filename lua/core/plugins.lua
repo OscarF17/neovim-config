d03041cb4c61c0db2b51b76207495198172d5a51
Oscar Flores <oscar.floresl@udem.e.du>
2023-04-10 17:11:15 -0600

    lualine, webicons, nerdfont

---
+++ lua/core/plugins.lua
@@ -0,0 +1,24 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Plugins
+local plugins = {
+    'nvim-lualine/lualine.nvim',
+    'nvim-tree/nvim-web-devicons'
+}
+
+-- Options
+local opts = {}
+
+require("lazy").setup(plugins, opts)
