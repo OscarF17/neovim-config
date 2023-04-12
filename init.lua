154b14f82aefdd8b271442ce9309990c3f7919cc
Oscar Flores <oscar.floresl@udem.e.du>
2023-04-12 00:27:00 -0600

    Removed lsps, added coc

--- init.lua
+++ init.lua
@@ -2,3 +2,8 @@ require("core.options")
 require("core.keymaps")
 require("core.plugins")
 require("core.conf_plugins")
+
+vim.cmd([[inoremap <silent><expr> <TAB> coc#pum#visible() ? coc#pum#next(1) : CheckBackspace() ? "<Tab>" : coc#refresh()]])
+vim.cmd([[inoremap <expr><S-TAB> coc#pum#visible() ? coc#pum#prev(1) : "<C-h>"]])
+
+
