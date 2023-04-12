632b04f3c238ea7c668375ec84062053f43eb828
Oscar Flores <oscar.floresl@udem.e.du>
2023-04-12 09:33:41 -0600

    Added coc extensions

---
+++ lua/core/conf_plugins/coc.lua
@@ -0,0 +1,14 @@
+-- List of coc extensions to install
+local extensions = {
+    "coc-html",
+    "coc-html-css-support",
+    "coc-git",
+    "coc-tsserver",
+    "coc-sql",
+    "coc-perl",
+    "coc-css",
+    "coc-clangd"
+}
+
+-- Set g:coc_global_extensions to the list of Coc extensions
+vim.g.coc_global_extensions = extensions
