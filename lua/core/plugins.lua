cadf64d9498a858ee1884fb5bdad3a2efdd76930
Oscar Flores <oscar.floresl@udem.e.du>
2023-04-12 14:01:39 -0600

    Uncommented friendly snippets

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -26,7 +26,7 @@ local plugins = {
     --'hrsh7th/cmp-nvim-lsp',
     --'L3MON4D3/LuaSnip',
     --'saadparwaiz1/cmp_luasnip',
-    --'rafamadriz/friendly-snippets',
+    'rafamadriz/friendly-snippets',
     --'williamboman/mason.nvim',
     --'williamboman/mason-lspconfig.nvim',
     --"neovim/nvim-lspconfig",
