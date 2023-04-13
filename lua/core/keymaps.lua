7668333755ee770d2939a7f7853a5eef35293cb3
Oscar Flores <oscar.floresl@udem.edu>
2023-04-12 20:08:16 -0600

    Copy and paste from clipboard

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -6,3 +6,9 @@ vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
 -- Forget search
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+
+-- Copy to clipboard on visual mode
+vim.api.nvim_set_keymap('v', '<Leader>y', '"+y', { noremap = true })
+
+-- Paste from clipboard on normal mode
+vim.api.nvim_set_keymap('n', '<Leader>p', '"+p', { noremap = true })
